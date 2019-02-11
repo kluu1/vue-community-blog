44dc0f8cf0e85d3b27c1c9225cac7993bc65f428
kevinluu <this.kevinluu@gmail.com>
2019-02-11 08:31:09 -0500

    setup boilerplate, vue-router, sytled login and signup

---
+++ src/components/Article.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="card my-3">
+    <div class="card-header font-weight-bold">{{ article.title }}</div>
+    <div class="card-body">
+      <div class="my-3" v-html="article.content.substring(0, 200)"></div>
+      
+      <router-link :to="`/article/${article.slug}`" class="btn btn-primary">Read more</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["article"]
+};
+</script>
+
